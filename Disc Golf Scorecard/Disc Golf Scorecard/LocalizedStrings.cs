e2cc28d82ebe1f156d9b00acc448b8fa4b5aea66
MattBubernak <soccerfreak24@gmail.com>
2014-06-04 22:10:35 -0600

    started everything!

---
+++ Disc Golf Scorecard/Disc Golf Scorecard/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Disc_Golf_Scorecard.Resources;
+
+namespace Disc_Golf_Scorecard
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
