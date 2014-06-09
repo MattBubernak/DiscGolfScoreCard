205e625c89be6188d40ee1a4b8d86be30a2bbc6a
MattBubernak <soccerfreak24@gmail.com>
2014-06-08 18:45:53 -0600

    added selection of players to scorecard

--- Disc Golf Scorecard/Disc Golf Scorecard/ViewModels/CreateScorecardViewModel.cs
+++ Disc Golf Scorecard/Disc Golf Scorecard/ViewModels/CreateScorecardViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Disc_Golf_Scorecard.ViewModels;
 using Disc_Golf_Scorecard.Models;
+using Disc_Golf_Scorecard.ViewModels;
+using System.Collections;
+using System.Diagnostics;
 
 namespace Disc_Golf_Scorecard.ViewModels
 {
     public class CreateScorecardViewModel : BaseViewModel
     {
         public HomePageViewModel homePageViewModel = null;
+
+        public ObservableCollection<PlayerViewModel> players= null; 
 
         private static CreateScorecardViewModel _instance = null;
 
@@ -21,6 +26,11 @@ namespace Disc_Golf_Scorecard.ViewModels
                 _instance = new CreateScorecardViewModel();
             }
             return _instance;
+        }
+
+        private CreateScorecardViewModel()
+        {
+            players = new ObservableCollection<PlayerViewModel>(); 
         }
 
 
--- Disc Golf Scorecard/Disc Golf Scorecard/Views/NewScorecard.xaml.cs
+++ Disc Golf Scorecard/Disc Golf Scorecard/Views/NewScorecard.xaml.cs
@@ -13,10 +13,18 @@ namespace Disc_Golf_Scorecard.Views
 {
     public partial class NewScorecard : PhoneApplicationPage
     {
+        int playerCount = 1; 
+
         public NewScorecard()
         {
             InitializeComponent();
-            CourseSelection.ItemsSource = HomePageViewModel.get_instance().courses; 
+            CourseSelection.ItemsSource = HomePageViewModel.get_instance().courses;
+            PlayerSelection1.ItemsSource = HomePageViewModel.get_instance().players;
+            PlayerSelection2.ItemsSource = HomePageViewModel.get_instance().players;
+            PlayerSelection3.ItemsSource = HomePageViewModel.get_instance().players;
+            PlayerSelection4.ItemsSource = HomePageViewModel.get_instance().players;
+            PlayerSelection5.ItemsSource = HomePageViewModel.get_instance().players; 
+
 
         }
 
@@ -25,9 +33,47 @@ namespace Disc_Golf_Scorecard.Views
 
         }
 
-        private void add_new_player(object sender, RoutedEventArgs e)
+        private void add_player(object sender, RoutedEventArgs e)
         {
+            playerCount++;
+            if (playerCount > 5)
+                playerCount = 5;
+            update_selectors(playerCount);
+        }
 
+        private void remove_player(object sender, RoutedEventArgs e)
+        {
+            playerCount--;
+            if (playerCount < 1)
+                playerCount = 1;
+            update_selectors(playerCount);
+        }
+
+        private void update_selectors(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    PlayerSelection2.Visibility = System.Windows.Visibility.Collapsed;
+                    break;
+                case 2:
+                    PlayerSelection2.Visibility = System.Windows.Visibility.Visible;
+
+                    PlayerSelection3.Visibility = System.Windows.Visibility.Collapsed;
+                    break;
+                case 3:
+                    PlayerSelection4.Visibility = System.Windows.Visibility.Collapsed;
+                    PlayerSelection3.Visibility = System.Windows.Visibility.Visible;
+
+                    break;
+                case 4:
+                    PlayerSelection4.Visibility = System.Windows.Visibility.Visible;
+                    PlayerSelection5.Visibility = System.Windows.Visibility.Collapsed;
+                    break;
+                case 5:
+                    PlayerSelection5.Visibility = System.Windows.Visibility.Visible;
+                    break;
+            }
         }
     }
 }
