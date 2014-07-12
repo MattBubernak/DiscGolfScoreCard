87a358b29552e1f44c1661edd1c209e10597cac9
MattBubernak <soccerfreak24@gmail.com>
2014-07-11 19:46:40 -0600

    player profile page now has basic functionality

--- Disc Golf Scorecard/Disc Golf Scorecard/Views/PlayerProfile.xaml.cs
+++ Disc Golf Scorecard/Disc Golf Scorecard/Views/PlayerProfile.xaml.cs
@@ -8,11 +8,18 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Disc_Golf_Scorecard.ViewModels;
+using Disc_Golf_Scorecard.Models;
+using Microsoft.Phone.Tasks;
+
 
 namespace Disc_Golf_Scorecard.Views
 {
     public partial class PlayerProfile : PhoneApplicationPage
     {
+
+        public PlayerViewModel player = null;
+        int recIndex = -1; 
+
         public PlayerProfile()
         {
             InitializeComponent();
@@ -27,9 +34,28 @@ namespace Disc_Golf_Scorecard.Views
                 if (NavigationContext.QueryString.TryGetValue("playerIndex", out selectedIndex))
                 {
                     int index = int.Parse(selectedIndex);
-                    DataContext = HomePageViewModel.get_instance().players[index];
+                    recIndex = index; 
+                    player = HomePageViewModel.get_instance().players[index];
+                    DataContext = player; 
                 }
             }
+        }
+
+        private void email_player(object sender, EventArgs e)
+        {
+            var task = new EmailComposeTask { To = player.player.PhoneNumber };
+            task.Show();
+        }
+
+        private void call_player(object sender, EventArgs e)
+        {
+            var task = new PhoneCallTask { PhoneNumber = player.player.PhoneNumber };
+            task.Show(); 
+        }
+
+        private void edit_player(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/NewPlayer.xaml?playerIndex=" + recIndex, UriKind.Relative));
         }
     }
 }
